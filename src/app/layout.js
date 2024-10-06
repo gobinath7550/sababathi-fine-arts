012238e49ca66052e0a3de759ae5d16900649384
gobinath7550 <gobinath7550@gmail.com>
2024-10-06 10:11:32 +0530

    Added the SEO content

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,14 +13,32 @@ const bebasNeueRegular = localFont({
     weight: "100 900",
 });
 
-export const metadata = {
-    title: "Sababathi Fine Arts",
-    description: "Sababathi Fine Arts",
-};
-
 export default function RootLayout({ children }) {
     return (
         <html lang="en" className="scroll-smooth">
+            <head>
+                <title>Sabapathi Fine Arts</title>
+                <meta
+                    name="description"
+                    content="Sabapathi Fine Arts – Welcome to Sabapathi Fine Arts, your trusted partner for premium printing solutions. We specialize in delivering high-quality, customized printing services to businesses and individuals. From business cards and brochures to large-format banners and packaging, our state-of-the-art equipment ensures sharp, vibrant, and durable prints. Explore our wide range of services and let us bring your vision to life with exceptional results, fast turnaround times, and outstanding customer service."
+                />
+                <meta
+                    name="keywords"
+                    content="Sabapathi Fine Arts, printing services, custom printing, professional printing, digital printing, offset printing, high-quality prints, business cards printing, brochures printing, posters printing, large format printing, banner printing, affordable printing services, custom design printing, personalized printing, promotional materials, print shop, professional print solutions, bulk printing services, creative printing solutions"
+                />
+                <meta name="author" content="Sabapathi" />
+                <meta name="robots" content="index, follow" />
+                <meta property="og:title" content="Sabapathi Fine Arts" />
+                <meta
+                    property="og:description"
+                    content="Sabapathi Fine Arts – Welcome to Sabapathi Fine Arts, your trusted partner for premium printing solutions. We specialize in delivering high-quality, customized printing services to businesses and individuals. From business cards and brochures to large-format banners and packaging, our state-of-the-art equipment ensures sharp, vibrant, and durable prints. Explore our wide range of services and let us bring your vision to life with exceptional results, fast turnaround times, and outstanding customer service."
+                />
+                <meta property="og:image" content="/images/main-logo.png" />
+                <meta
+                    property="og:url"
+                    content="https://sababathi-fine-arts.vercel.app/"
+                />
+            </head>
             <body
                 className={`${geistMonoVF.variable} ${bebasNeueRegular.variable} antialiased`}
             >
